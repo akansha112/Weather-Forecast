3a6121babba8c4708f03b0c47410e0359db25295
akansha014 <93869043+akansha014@users.noreply.github.com>
2023-03-20 21:38:13 +0530

    Final Commit

---
+++ app/src/main/java/com/akanshasaxena/weatherapp/location/LocationCord.java
@@ -0,0 +1,8 @@
+package com.akanshasaxena.weatherapp.location;
+
+public class LocationCord {
+    public static String lat = "";
+    public static String lon = "";
+     public final static String API_KEY = "0bc14881636d2234e8c17736a470535f";
+}
+
